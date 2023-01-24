48c6d1ccfd3e8143005f3f686230e1f669d202fb
callumleys <callumleys@icloud.com>
2023-01-24 18:46:59 +0000

    Added selector feature - allows user to select job Refactored some js code

--- priceCalculator/calculatePrice.js
+++ priceCalculator/calculatePrice.js
@@ -1,11 +1,17 @@
 function calculatePrice() {
-    var s = document.getElementById("salary").value;
-    var d = document.getElementById("days").value;
+    var salary = document.getElementById("salary").value;
+    var days = document.getElementById("days").value;
     console.log("Calculating price")
-    console.log(s)
-    console.log(d)
+    console.log(salary)
+    console.log(days)
     let finalPrice = 0;
-    dailyRate = s/365;
-    finalPrice = dailyRate * d;
-    document.getElementById("finalPrice").innerHTML = finalPrice;
+    dailyRate = salary/365;
+    finalPrice = Math.round(dailyRate * days/50) * 50;
+    priceFormatted = finalPrice.toLocaleString('en-UK');
+    document.getElementById("finalPrice").innerHTML = "\u00A3" + priceFormatted;
 }
+function updateSalary() {
+    var jobOptions = document.getElementById("job-options");
+    var selectedSalary = jobOptions.options[jobOptions.selectedIndex].value;
+    document.getElementById("salary").value = selectedSalary;
+  }
