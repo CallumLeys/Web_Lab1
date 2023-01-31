400cbc0981a49ed030a10143190cd19140a9a2b2
callumleys <callumleys@icloud.com>
2023-01-31 17:52:09 +0000

    backend attempts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
@@ -10,6 +11,36 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+// 404 error handling
 app.use(function(req, res, next) {
-  res.status(404).send("Sorry, we couldn't find that page!");
+  res.status(404).render('views/404.html');
 });
+
+/*
+// Handling request 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('index/:salary/:days', (req, res) => {
+    const salary = req.params.salary;
+    const days = req.params.days;
+
+    console.log("Calculating price");
+    console.log(salary);
+    console.log(days);
+
+    // Get the numbers from the request body
+    const salary = req.body.salary;
+    const days = req.body.days;
+    console.log(salary)
+    console.log(days)
+
+    // Calculate the sum
+    let finalPrice = 0;
+    dailyRate = salary/365;
+    finalPrice = Math.round(dailyRate * days/50) * 50;
+    priceFormatted = finalPrice.toLocaleString('en-UK');
+
+    // Send the sum back as a response
+    res.json({ priceFormatted });
+*/
--- public/calculatePrice.js
+++ public/calculatePrice.js
@@ -1,14 +1,23 @@
 function calculatePrice() {
     var salary = document.getElementById("salary").value;
     var days = document.getElementById("days").value;
-    console.log("Calculating price")
-    console.log(salary)
-    console.log(days)
+
+    /*
+    fetch('api/${salary}/${days}')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        // Update the UI with the sum
+        document.getElementById("finalPrice").innerHTML = "\u00A3" + req.query.days;
+      });
+      */
+    // Calculate the sum
     let finalPrice = 0;
     dailyRate = salary/365;
     finalPrice = Math.round(dailyRate * days/50) * 50;
     priceFormatted = finalPrice.toLocaleString('en-UK');
     document.getElementById("finalPrice").innerHTML = "\u00A3" + priceFormatted;
+
 }
 function updateSalary() {
     var jobOptions = document.getElementById("job-options");
